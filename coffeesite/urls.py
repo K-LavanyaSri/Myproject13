6426095f6d6bf477f2e07f5e0d8899f2ea088849
Lavanyakonduri <lavanyasrichowdary59@gmail.com>
2024-12-22 20:05:05 +0530

    add project

--- coffeesite/urls.py
+++ coffeesite/urls.py
@@ -5,7 +5,7 @@ app_name='coffeesite'
 urlpatterns = [
     path('',home,name='home'),
     path('products/',products,name='products'),
-    path('testimonials/',testimonials,name='testimonials'),
-    path('aboutUs/',aboutUs,name='aboutUs'),
-    path('contactUs/',contactUs,name='contactUs'),
+    # path('testimonials/',testimonials,name='testimonials'),
+    # path('aboutUs/',aboutUs,name='aboutUs'),
+    # path('contactUs/',contactUs,name='contactUs'),
 ]
